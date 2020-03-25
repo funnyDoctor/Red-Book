fea5ca62ba266d45b3b1a6435488a5940533e344
funnyDoctor <raskol888i@gmail.com>
2020-03-25 14:35:08 +0300

    Complited app next

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Page from "./components/Page box/Page/Page";
+import Signin from "./components/Log folder/Signin/Signin";
+import Register from "./components/Log folder/Register/Register";
+import Navigation from "./components/Log folder/Navigation/Navigation";
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            route: 'signin',
+            isSignedIn: false,
+            user: {
+                id: '',
+                name: '',
+                email: '',
+                joined: ''
+            }
+        }
+    }
+
+    loadUser = (data) => {
+        this.setState({user: {
+                id: data.id,
+                name: data.name,
+                email: data.email,
+                joined: data.joined
+            }})
+    };
+
+    onRouteChange = (route) => {
+        if (route === 'signout') {
+            this.setState({isSignedIn: false})
+        } else if (route === 'home') {
+            this.setState({isSignedIn: true})
+        }
+        this.setState({route: route});
+    };
+
+    render() {
+        const { isSignedIn, route } = this.state;
+        return (
+            <div className="App">
+                <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+                {route === 'home' ?
+                    <Page/> :
+                    (
+                        route === "signin" ?
+                            <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>:
+                            <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+                    )
+                }
+            </div>
+        )
+    }
 }
 
 export default App;
